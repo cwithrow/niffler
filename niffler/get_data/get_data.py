825b55a7970b70c73f19510dff654084f9f29ce0
cwithrow <candice.withrow@gmail.com>
2019-02-05 11:04:40 -0600

    Added logging and initial logic to grab posts

--- niffler/get_data/get_data.py
+++ niffler/get_data/get_data.py
@@ -1,5 +1,52 @@
 import facebook
 import config
+import logging
+import json
+
+def get_comments(obj_id, graph_obj):
+
+    comments = graph_obj.request('obj_id/?fields=comments')
 
 
-graph = Facebook.GraphAPI(access_token=config.fb_token, version = 2.7)
+
+
+
+
+# Get comments while there are any
+
+
+
+# How to store and attach to
+
+
+
+def main():
+
+    logging.basicConfig(level=logging.DEBUG,filename='app.log', filemode='w',
+                        format='%(name)s - %(levelname)s - %(message)s')
+
+    try:
+        graph = facebook.GraphAPI(access_token=config.fb_token, version='3.1')
+    except facebook.GraphAPIError:
+        logging.error("facebook.GraphAPIError occurred", exc_info=True)
+
+    date1 = '01/20/2019'
+    date2 = '01/27/2019'
+
+    if graph:
+        try:
+          posts = graph.request(config.group_id+'/?fields=feed.since('+date1+').until(' + date2 +')')
+        except facebook.GraphAPIError:
+            logging.error("facebook.GraphAPIError occurred", exc_info=True)
+
+    if posts:
+
+        print(len(posts['feed']['data']))
+        # for p in posts['feed']['data']:
+        #     p['comments'] = get_comments(p['id'])
+
+
+
+
+if __name__ == '__main__':
+    main()
