a20b7368ed09c59dc534896998a93e984a0c8c85
cwithrow <candice.withrow@gmail.com>
2019-02-05 18:21:02 -0600

    function/script to extract all posts, comments and replies from group feed

--- niffler/get_data/get_data.py
+++ niffler/get_data/get_data.py
@@ -2,10 +2,20 @@ import facebook
 import config
 import logging
 import json
+import os
 
 def get_comments(obj_id, graph_obj):
 
-    comments = graph_obj.request('obj_id/?fields=comments')
+    comments = graph_obj.request(obj_id+'/?fields=comments')
+
+    for c in comments['comments']['data']:
+        try:
+            c['replies']=graph_obj.request(c['id']+'/?fields=comments')
+        except KeyError():
+            logging.error("get_comments error: No replies", exc_info=True)
+
+    # print(comments)
+    return comments
 
 
 
@@ -25,28 +35,38 @@ def main():
     logging.basicConfig(level=logging.DEBUG,filename='app.log', filemode='w',
                         format='%(name)s - %(levelname)s - %(message)s')
 
+# Create facebook graphAPI object to connect and retrieve data
+# token and group Id set in config.py
     try:
         graph = facebook.GraphAPI(access_token=config.fb_token, version='3.1')
     except facebook.GraphAPIError:
         logging.error("facebook.GraphAPIError occurred", exc_info=True)
 
-    date1 = '01/20/2019'
-    date2 = '01/27/2019'
+# May add date options to cl inputs, then could also be regularly run/updated
+    date1 = '01/01/2019'
+    date2 = '02/05/2019'
 
     if graph:
         try:
-          posts = graph.request(config.group_id+'/?fields=feed.since('+date1+').until(' + date2 +')')
+            posts = graph.request(config.group_id+'/?fields=feed.since('+date1+').until(' + date2 +')')
         except facebook.GraphAPIError:
             logging.error("facebook.GraphAPIError occurred", exc_info=True)
 
     if posts:
 
-        print(len(posts['feed']['data']))
-        # for p in posts['feed']['data']:
-        #     p['comments'] = get_comments(p['id'])
+        try:
+            for p in posts['feed']['data']:
+                p['comments'] = get_comments(p['id'], graph)['comments']
+
+        except facebook.GraphAPIError:
+            logging.error("facebook.GraphAPIError occurred", exc_info=True)
 
 
+# export to json/csv
 
+    with open(os.path.join(config.output_path,'posts '+ date2.replace('/','_') +'.json'), 'w') as f:
+        json.dump(posts, f)
+#-------------------------------------#
 
 if __name__ == '__main__':
     main()
