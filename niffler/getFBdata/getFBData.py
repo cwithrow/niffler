c90e2f43767dc6fcc3e07111db76758d3c8c1170
cwithrow <candice.withrow@gmail.com>
2019-03-05 11:38:38 -0600

    Changed code to allow option to extract different types and load object ids from file

--- niffler/getFBdata/getFBData.py
+++ niffler/getFBdata/getFBData.py
@@ -24,6 +24,12 @@ class FBData():
                 p.pop('id', None)
                 p['post'] = p['message']
                 p.pop('message', None)
+
+            with open(os.path.join(config.output_path, '/' + end_date.replace('/', '_') + '/' +'posts' +
+                    end_date.replace('/', '_') + '.json'), 'w') as f:
+                json.dump(posts, f)
+
+            print('Posts saved\n ')
             return posts
 
     def get_comments(self, obj_id, graph_obj, limit, delay):
@@ -48,6 +54,20 @@ class FBData():
         # print(comments)
             return comments
 
+    def get_all_comments(self,posts_list, graph_obj, end_date, limit, delay):
+        comments = []
+
+        for p in posts_list:
+            comments = comments + self.get_comments(p['post_id'], graph_obj, limit, delay)
+
+        with open(os.path.join(config.output_path,  '/' +  end_date.replace('-', '_') + '/' + 'comments' +
+                end_date.replace('-', '_') + '.json'), 'w') as f:
+            json.dump(comments, f)
+
+        print('Comments saved\n ')
+
+        return comments
+
     def get_replies(self,post_id,obj_id, graph_obj, limit, delay):
         time.sleep(delay)
         try:
@@ -69,18 +89,27 @@ class FBData():
 
             return replies
 
-
-    def get_data(self,graph_obj,beg_date, end_date,limit, delay=18):
-        posts = self.get_posts(graph_obj,beg_date,end_date, limit, delay)
-
-
-        comments = []
+    def get_all_replies(self,comments_list, graph_obj, end_date, limit, delay):
         replies = []
-        for p in posts:
-            comments = comments + self.get_comments(p['post_id'], graph_obj, limit, delay)
+        for c in comments_list:
+            replies = replies + self.get_replies(c['post_id'], c['comm_id'], graph_obj, limit, delay)
+            print(len(replies))
 
-        for c in comments:
-            replies = replies + self.get_replies(c['post_id'],c['comm_id'],graph_obj,limit,delay)
+        with open(os.path.join(config.output_path, end_date.replace('-', '_') + '/' +'replies' +
+                end_date.replace('-', '_') + '.json'), 'w') as f:
+            json.dump(replies, f)
+
+        print('Replies saved\n ')
+
+        return replies
+
+
+    def get_all_data(self,graph_obj,beg_date, end_date,limit, delay=18):
+        posts = self.get_posts(graph_obj,beg_date,end_date, limit)
+
+        comments = self.get_all_comments(posts, graph_obj, end_date, limit, delay)
+
+        replies = self.get_all_comments(comments, graph_obj, end_date, limit, delay)
 
         return posts, comments, replies
 
@@ -89,9 +118,21 @@ def main():
     date2 = '2019-02-28'
     lim = '100'
     delay = 18
+
+    # options: 'a' for all, 'p' for posts, 'c' for comments, 'r' for replies
+    option = 'r'
+    #file path for input file for c or r options
+    inputfp = config.output_path
+
     # initialize logger
     logging.basicConfig(level=logging.DEBUG,filename='app.log', filemode='w',
                         format='%(name)s - %(levelname)s - %(message)s')
+
+
+    #
+    # parser = get_argparser()
+    # args = parser.parse_args()
+
 
 # Create facebook graphAPI object to connect and retrieve data
 # token and group Id set in config.py
@@ -101,36 +142,34 @@ def main():
         logging.error("facebook.GraphAPIError connection error", exc_info=True)
     else:
         fbd = FBData()
-    # Extract data for posts table
-    #     posts_list, comments_list, replies_list = fbd.get_data( graph, date1, date2, lim,delay)
 
-        posts = fbd.get_posts(graph, date1, date2, lim)
+        if option == 'all':
+         # Extract all tables
+            fbd.get_all_data( graph, date1, date2, lim,delay)
 
-        with open(os.path.join(config.output_path, 'posts' + date2.replace('/', '_') + '.json'), 'w') as f:
-            json.dump(posts, f)
+        elif option == 'p':
 
-        print('Posts saved\n ')
+            fbd.get_posts(graph, date1, date2, lim)
 
-    # Extract data for comments table
-        comments = []
+        elif option == 'c':
+            with open(os.path.join(inputfp + '/' + date2.replace('-', '_') + '/' +'posts' +
+                         date2.replace('-', '_') + '.json'), 'r') as f:
+                posts = json.load(f)
+     # Extract data for comments table
+            fbd.get_all_comments(posts,graph, date2, lim, delay)
 
-        for p in posts:
-            comments = comments + fbd.get_comments(p['post_id'], graph, lim, delay)
+        elif option == 'r':
+            with open(os.path.join(inputfp +  '/' +  date2.replace('-', '_') + '/' + 'comments' +
+                                           date2.replace('-', '_') + '.json'), 'r') as f:
+                comments = json.load(f)
+            # Extract data for replies table
+            fbd.get_all_replies(comments, graph, date2, lim, delay)
 
-        with open(os.path.join(config.output_path, 'comments' + date2.replace('/', '_') + '.json'), 'w') as f:
-            json.dump(comments, f)
 
-        print('Comments saved\n ')
 
-    # Extract data for replies table
-        replies = []
-        for c in comments:
-            replies = replies + fbd.get_replies(c['post_id'], c['comm_id'], graph, lim, delay)
 
-        with open(os.path.join(config.output_path,'replies'+ date2.replace('/','_') +'.json'), 'w') as f:
-            json.dump(replies, f)
 
-        print('Replies saved\n ')
+
 #-------------------------------------#
 
 if __name__ == '__main__':
